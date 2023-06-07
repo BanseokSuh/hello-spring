d320c12a60ece7d97bfc71664731007aa9aa0723
BanseokSuh <still3028@gmail.com>
2023-06-07 20:15:57 +0900

    다형성을 활용한 확장성 관련 주석, 개방-폐쇄 원칙 주석

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -24,10 +24,13 @@ public class SpringConfig {
         return new MemberService(memberRepository());
     }
 
+    // 객체지향에서의 다형성을 활용함 -> 예를 들어, 인터페이스를 두고, 구현체를 바꿔끼기
+    // 다른 코드를 수정할 필요 없이 설정 파일만 수정함 -> 스프링의 장점
+    // 개방-폐쇄 원칙: 확장에는 열려잇고, 수정에는 닫혀있음
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        return new JdbcMemberRepository(dataSource); // 구현 클래스 바꿔치기
     }
 
 }
